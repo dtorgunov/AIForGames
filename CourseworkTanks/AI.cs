8a333f3298a8192c02622dd5a7ceb487c769929c
Denis Torgunov <dtorgunov@leafonthewind.net>
2016-05-10 00:37:57 +0100

    Initial commit
    Added the basic file struture and a working, if simple, AI
    that just moves upwards.

---
+++ CourseworkTanks/AI.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GridWorld
+{
+    public class dtorguno : BasePlayer
+    {
+        private PlayerWorldState worldState;
+
+        public dtorguno()
+            : base()
+        {
+            this.Name = "Subsumptive AI";
+        }
+
+        public override ICommand GetTurnCommands(IPlayerWorldState igrid)
+        {
+            return new Command(Command.Move.Up, true);
+        }
+    }
+}
