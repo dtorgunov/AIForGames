cc36167f3af790216d216f3fd76d800cddb6aa27
George Bell <george@cattlebell.com>
2016-05-10 14:35:49 +0100

    Basic framework added to LocationLocator.

--- CourseworkTanks/LocationLocator.cs
+++ CourseworkTanks/LocationLocator.cs
@@ -7,5 +7,31 @@ namespace GridWorld
 {
     class LocationLocator
     {
+
+        private Cell[,] localMap;
+
+        /// <summary>
+        /// Constructs the class.
+        /// </summary>
+        /// <param name="localMap">Map of the game environment.</param>
+        public LocationLocator(Cell[,] localMap)
+        {
+            this.localMap = localMap;
+        }
+
+        /// <summary>
+        /// Updates the localMap
+        /// </summary>
+        /// <param name="localMap">Map of the game environment.</param>
+        public void Update(Cell[,] localMap)
+        {
+            this.localMap = localMap;
+        }
+
+        public Cell Retreat()
+        {
+            
+        }
+
     }
 }
