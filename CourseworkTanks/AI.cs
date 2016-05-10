45fa195910765b3ee28cccc222cc48459c645831
Denis Torgunov <dtorgunov@leafonthewind.net>
2016-05-10 01:06:07 +0100

    Add the subsumption dispatcher class
    Made the class an inner class of the main class, just to make
    it easier to manage, and as I was not sure how multiclass things
    would work in a "class library" setting.

--- CourseworkTanks/AI.cs
+++ CourseworkTanks/AI.cs
@@ -19,5 +19,49 @@ namespace GridWorld
         {
             return new Command(Command.Move.Up, true);
         }
+
+        class SubsumptionDispatch
+        {
+            public delegate bool Situation();
+            public delegate ICommand Action();
+
+            // the FIRST action in the table has HIGHEST priority
+            private List<Tuple<Situation, Action>> dispatchTable;
+
+            public SubsumptionDispatch()
+            {
+                this.dispatchTable = new List<Tuple<Situation, Action>>();
+            }
+
+            public SubsumptionDispatch(List<Tuple<Situation, Action>> dispatchTable)
+            {
+                this.dispatchTable = dispatchTable;
+            }
+
+            // adds as the LOWEST PRIORITY action
+            public void add(Tuple<Situation, Action> entry)
+            {
+                dispatchTable.Add(entry);
+            }
+
+            public static bool defaultAction()
+            {
+                return true;
+            }
+
+            public ICommand act()
+            {
+                foreach (Tuple<Situation, Action> behaviour in dispatchTable)
+                {
+                    if (behaviour.Item1())
+                    {
+                        return behaviour.Item2();
+                    }
+                }
+
+                // isn't reachable if the default action is given
+                return null;
+            }
+        }
     }
 }
