1ca0096737b84946b64e4c6b6954447b2aec23d8
George Bell <george@cattlebell.com>
2016-05-10 14:49:01 +0100

    Added return type and base case to Retreat method.

--- CourseworkTanks/LocationLocator.cs
+++ CourseworkTanks/LocationLocator.cs
@@ -15,25 +15,32 @@ namespace GridWorld
         /// Constructs the class.
         /// </summary>
         /// <param name="localMap">Map of the game environment.</param>
+        /// <param name="hero">Location of the player.</param>
         public LocationLocator(Cell[,] localMap, GridSquare hero)
         {
-            this.localMap = localMap;
-            this.hero = hero;
+            Update(localMap, hero);
         }
 
         /// <summary>
         /// Updates the localMap
         /// </summary>
         /// <param name="localMap">Map of the game environment.</param>
+        /// <param name="hero">Location of the player.</param>
         public void Update(Cell[,] localMap, GridSquare hero)
         {
             this.localMap = localMap;
             this.hero = hero;
         }
 
-        public /*WhateverConnorNeeds*/ Retreat()
+
+        public Tuple<int, int> Retreat()
         {
+            int hX = hero.X;
+            int hY = hero.Y;
+
             
+            //Base case. Should never be reached.
+            return new Tuple<int,int>(0, 0);
         }
 
     }
